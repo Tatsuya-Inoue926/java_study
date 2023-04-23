72e560cd93204937734d98b9d5ef90fae7963da2
Tatsuya <t.i.ykts.926@gmail.com>
2023-04-23 17:18:42 +0900

    変更

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,18 @@
 public class Calculator {
-
+    //オーバーロードはcalcという同じものでも引数の数によってパターンを分けられる
+    //引数が1のパターン
     public int calc(String str1){
         int intNum = Integer.parseInt(str1);
         intNum *= intNum;
+        System.out.println("引数1："+ intNum);
         return intNum;
     }
+    //引数が2のパターン
     public int calc(String str1, String str2){
         int intNum = Integer.parseInt(str2);
         int intNum2 = Integer.parseInt(str1);
         intNum *= intNum2;
+        System.out.println("引数2："+ intNum);
         return intNum;
     }
 }
--- JavaExam3_4.java
+++ JavaExam3_4.java
@@ -3,8 +3,8 @@ public class JavaExam3_4 {
     public static void main(String[] args){
         Calculator cl = new Calculator();
         //戻り値を受け取るには変数を使う
-        int result = cl.calc("1000","50");
-        System.out.println("引数が2つのメソッド：" + result);
+        /*int result = */cl.calc("1000","200");
+        //System.out.println("引数が2つのメソッド：" + result);
     }
     
 }
